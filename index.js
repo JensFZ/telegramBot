02bf60a2a48494acb166fbf0b64550ca2300e798
Jens Fischer <JensFZ@gmail.com>
2021-01-08 01:07:07 +0100

    added .env support

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 const TelegramBot = require('node-telegram-bot-api');
 const fortune = require('fortune-teller');
 const ping = require('ping');
 const nslookup = require('nslookup');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '';
+const token = process.env.KEY;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
